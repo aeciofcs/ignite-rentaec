360d31e1cc0601b904bba899de72cc0d70d4ad7b
Aecio Silva <aeciofcs@gmail.com>
2021-04-24 10:03:03 -0300

    Adicionando autenticação nas rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { authenticateRoutes } from "./Authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./Specification.routes";
@@ -7,9 +8,9 @@ import { usersRoutes } from "./Users.routes";
 
 const router = Router();
 
-router.use("/categories", categoriesRoutes);
-router.use("/specifications", specificationsRoutes);
-router.use("/users", usersRoutes);
+router.use("/categories", ensureAuthenticated, categoriesRoutes);
+router.use("/specifications", ensureAuthenticated, specificationsRoutes);
+router.use("/users", ensureAuthenticated, usersRoutes);
 router.use(authenticateRoutes);
 
 export { router };
