5937fa757c23f462d9381344f0b9efbd75aec56a
Aecio Silva <aeciofcs@gmail.com>
2021-06-20 11:15:37 -0300

    Cap 4 - Estruturando a entidade Carros.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,10 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/Infra/TypeORM/Repositories/UsersRepository";
 import { IUserRepository } from "@modules/accounts/Repositories/IUserRepository";
+import { CarsRepository } from "@modules/cars/Infra/TypeORM/Repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/Infra/TypeORM/Repositories/CategoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/Infra/TypeORM/Repositories/SpecificationsRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
@@ -21,3 +23,5 @@ container.registerSingleton<IUserRepository>(
     "UsersRepository",
     UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
