8df3404b32cbb230a05eb2481a965b29f0455c72
Aecio Silva <aeciofcs@gmail.com>
2021-06-20 11:23:37 -0300

    Cap 4 - Criando a rota da entidade Carros.

--- src/shared/Infra/HTTP/routes/index.ts
+++ src/shared/Infra/HTTP/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { ensureAuthenticated } from "@shared/Infra/HTTP/middlewares/ensureAuthenticated";
 import { categoriesRoutes } from "@shared/Infra/HTTP/routes//categories.routes";
 import { authenticateRoutes } from "@shared/Infra/HTTP/routes/Authenticate.routes";
+import { carsRoutes } from "@shared/Infra/HTTP/routes/Cars.routes";
 import { specificationsRoutes } from "@shared/Infra/HTTP/routes/Specification.routes";
 import { usersRoutes } from "@shared/Infra/HTTP/routes/Users.routes";
 
@@ -11,6 +12,7 @@ const router = Router();
 router.use("/categories", ensureAuthenticated, categoriesRoutes);
 router.use("/specifications", ensureAuthenticated, specificationsRoutes);
 router.use("/users", ensureAuthenticated, usersRoutes);
+router.use("/cars", ensureAuthenticated, carsRoutes);
 router.use(authenticateRoutes);
 
 export { router };
