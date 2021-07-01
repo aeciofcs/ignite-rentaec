78137a9e65aa5a4844b7fb37207481dbd348452c
Aecio Silva <aeciofcs@gmail.com>
2021-06-30 21:39:35 -0300

    Cap 4 - Finalizando testes de integração.

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -8,7 +8,7 @@ import createConnection from "@shared/Infra/TypeORM";
 
 let connection: Connection;
 
-describe("Create Category Controller", () => {
+describe("List Categories", () => {
     beforeAll(async () => {
         connection = await createConnection();
         await connection.runMigrations();
@@ -27,7 +27,7 @@ describe("Create Category Controller", () => {
         await connection.close();
     });
 
-    it("should be able to create a new category", async () => {
+    it("should be able to list all categories", async () => {
         const responseToken = await request(app).post("/sessions").send({
             email: "admin@rentaec.com.br",
             password: "admin",
@@ -35,37 +35,25 @@ describe("Create Category Controller", () => {
 
         const { token } = responseToken.body;
 
-        const response = await request(app)
+        await request(app)
             .post("/categories")
             .send({
-                name: "Category SuperTest",
-                description: "Description Category SuperTest",
+                name: "Category Supertest",
+                description: "Description Category Supertest",
             })
             .set({
                 Authorization: `Bearer ${token}`,
             });
 
-        expect(response.status).toBe(201);
+        const response = await request(app).get("/categories");
+
+        // console.log(response.body);
+
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(1);
+        expect(response.body[0]).toHaveProperty("id");
+        expect(response.body[0].name).toEqual("Category Supertest");
     });
 
-    it("should not be able to create a new category with name exists", async () => {
-        const responseToken = await request(app).post("/sessions").send({
-            email: "admin@rentaec.com.br",
-            password: "admin",
-        });
-
-        const { token } = responseToken.body;
-
-        const response = await request(app)
-            .post("/categories")
-            .send({
-                name: "Category SuperTest",
-                description: "Description Category SuperTest",
-            })
-            .set({
-                Authorization: `Bearer ${token}`,
-            });
-
-        expect(response.status).toBe(400);
-    });
+    // it("should not be able to create a new category with name exists", async () => {});
 });
