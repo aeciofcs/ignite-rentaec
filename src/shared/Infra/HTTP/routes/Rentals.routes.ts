c7f1599955a33268759594510000075eae317b6e
Aecio Silva <aeciofcs@gmail.com>
2021-07-07 10:30:23 -0300

    Cap 5 - Listar Rentals por Usuário.

--- src/shared/Infra/HTTP/routes/Rentals.routes.ts
+++ src/shared/Infra/HTTP/routes/Rentals.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 import { ensureAdmin } from "@shared/Infra/HTTP/middlewares/ensureAdmin";
 import { ensureAuthenticated } from "@shared/Infra/HTTP/middlewares/ensureAuthenticated";
 
@@ -9,6 +10,7 @@ const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post(
     "/",
@@ -22,6 +24,13 @@ rentalRoutes.post(
     ensureAuthenticated,
     ensureAdmin,
     devolutionRentalController.handle
+);
+
+rentalRoutes.get(
+    "/user",
+    ensureAuthenticated,
+    ensureAdmin,
+    listRentalsByUserController.handle
 );
 
 export { rentalRoutes };
