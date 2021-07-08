bdde17235a82ebef356fc0f1632d6e56cc61115a
Aecio Silva <aeciofcs@gmail.com>
2021-07-07 21:10:07 -0300

    Cap 5 - Lógica use case refresh token.

---
+++ src/modules/accounts/UseCases/refreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,63 @@
+import { sign, verify } from "jsonwebtoken";
+import { inject } from "tsyringe";
+
+import Auth from "@config/Auth";
+import { UserTokens } from "@modules/accounts/Infra/TypeORM/Entities/UserTokens";
+import { IUsersTokensRepository } from "@modules/accounts/Repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/Providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+interface IPayLoad {
+    sub: string;
+    email: string;
+}
+
+class RefreshTokenUseCase {
+    constructor(
+        @inject("UsersTokensRepository")
+        private usersTokensRepository: IUsersTokensRepository,
+
+        @inject("DayJSDateProvider")
+        private dayJsDateProvider: IDateProvider
+    ) {}
+    async execute(token: string): Promise<string> {
+        // Decodifica o token pegando o email e o id do usuário.
+        const { email, sub: user_id } = verify(
+            token,
+            Auth.secret_refresh_token
+        ) as IPayLoad;
+
+        // verifica se o token existe.
+        const userToken = await this.usersTokensRepository.findByUserIdAndRefreshToken(
+            user_id,
+            token
+        );
+
+        if (!userToken) {
+            throw new AppError("Refresh Token does not exists!");
+        }
+
+        // Se existir , remove do banco de dados.
+        await this.usersTokensRepository.deleteById(userToken.id);
+
+        // Gerar novamente um novo refresh token para o usuario.
+        const refresh_token = sign({ email }, Auth.secret_refresh_token, {
+            subject: user_id,
+            expiresIn: Auth.expires_in_refresh_token,
+        });
+
+        const expires_date = this.dayJsDateProvider.addDays(
+            Auth.expires_refresh_token_days
+        );
+
+        await this.usersTokensRepository.create({
+            user_id,
+            refresh_token,
+            expires_date,
+        });
+
+        return refresh_token;
+    }
+}
+
+export { RefreshTokenUseCase };
